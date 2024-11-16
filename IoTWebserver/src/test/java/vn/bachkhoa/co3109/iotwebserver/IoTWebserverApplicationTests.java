16783370c61625370dafcce9ac4877c71f80d13f
hoaqdzink2312 <vinhnh.2312@gmail.com>
2024-11-16 12:38:11 +0700

    Add BE and rename folder FE

---
+++ IoTWebserver/src/test/java/vn/bachkhoa/co3109/iotwebserver/IoTWebserverApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.bachkhoa.co3109.iotwebserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IoTWebserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
