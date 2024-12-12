4bd8d22922c20e067e90d13a191e8257264bbda3
hoaqdzink <vinh.nguyenbk7979@hcmut.edu.vn>
2024-12-12 08:58:25 +0700

    push be

---
+++ IoTWebServerBE/src/test/java/com/example/iotwebserverbe/IoTWebServerBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.iotwebserverbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IoTWebServerBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
